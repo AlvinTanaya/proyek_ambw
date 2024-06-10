c6d97aef9eb9314341dab7d9718325d118be9a57
AlvinTanaya <alvintanaya2002@gmail.com>
2024-06-10 20:33:28 +0700

    vidio alfon ajg

--- lib/screens/addPost_screen.dart
+++ lib/screens/addPost_screen.dart
@@ -1,12 +1,90 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
 import 'base_screen.dart';
 
-class AddPostScreen extends StatelessWidget {
+class AddPostScreen extends StatefulWidget {
+  @override
+  _AddPostScreenState createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  File? _videoFile;
+  VideoPlayerController? _controller;
+  bool _isVideoLoading = false;
+  bool _isVideoPlaying = false;
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickVideo() async {
+    final pickedFile =
+        await ImagePicker().getVideo(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _isVideoLoading = true;
+      });
+
+      File videoFile = File(pickedFile.path);
+      _controller = VideoPlayerController.file(videoFile)
+        ..initialize().then((_) {
+          setState(() {
+            _videoFile = videoFile;
+            _isVideoLoading = false;
+            _isVideoPlaying = true; // Autoplay the video
+          });
+        })
+        ..setLooping(true) // Set the video to loop
+        ..addListener(() {
+          setState(() {});
+        })
+        ..play();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Post'),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text('Add Post Screen'),
+        child: _videoFile != null
+            ? AspectRatio(
+                aspectRatio: _controller!.value.aspectRatio,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    VideoPlayer(_controller!),
+                    _controller!.value.isInitialized &&
+                            !_controller!.value.isPlaying
+                        ? IconButton(
+                            icon: Icon(Icons.play_arrow),
+                            onPressed: () {
+                              setState(() {
+                                _isVideoPlaying = true;
+                                _controller!.play();
+                              });
+                            },
+                          )
+                        : Container(),
+                  ],
+                ),
+              )
+            : _isVideoLoading
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: _pickVideo,
+                    child: Text('Pick a video'),
+                  ),
       ),
       bottomNavigationBar: BaseScreen(currentIndex: 2),
     );
